225a535967fb2eab50f59d347d46b0a49aa829d5
Avmur <avblag@mail.ru>
2021-01-21 21:54:41 +0900

    Task 7

--- app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
+++ app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
@@ -29,4 +29,49 @@ object Utils {
         } else
             firstLetter.toString() + lastLetter.toString()
     }
+
+    fun transliteration(payload: String, divider: String = " "): String {
+        val dictionary = mapOf(
+            "а" to "a",
+            "б" to "b",
+            "в" to "v",
+            "г" to "g",
+            "д" to "d",
+            "е" to "e",
+            "ё" to "e",
+            "ж" to "zh",
+            "з" to "z",
+            "и" to "i",
+            "й" to "i",
+            "к" to "k",
+            "л" to "l",
+            "м" to "m",
+            "н" to "n",
+            "о" to "o",
+            "п" to "p",
+            "р" to "r",
+            "с" to "s",
+            "т" to "t",
+            "у" to "u",
+            "ф" to "f",
+            "х" to "h",
+            "ц" to "c",
+            "ч" to "ch",
+            "ш" to "sh",
+            "щ" to "sh'",
+            "ъ" to "",
+            "ы" to "i",
+            "ь" to "",
+            "э" to "e",
+            "ю" to "yu",
+            "я" to "ya",
+            " " to divider
+        )
+        var output = payload
+        for ((rLetter, lLetter) in dictionary) {
+            output = output.replace(rLetter, lLetter)
+        }
+        return output
+    }
+
 }
